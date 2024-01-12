58923556a751554d1ff53405604c2edee9cf55e8
Shaquil F. Daude <shaquil.faizal@gmail.com>
2024-01-12 21:35:59 +0200

    get payloads and send requests

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 )
 
@@ -24,20 +25,33 @@ func main() {
 	configs := []Config{}
 
 	content, err := os.ReadFile(configFile)
+	requestCount := 10
 
-	if err != nil {
-		fmt.Println("Error reading file", err)
-		return
-	}
+	for i := 0; i < requestCount; i++ {
 
-	err = json.Unmarshal(content, &configs)
-	if err != nil {
-		fmt.Println("Error unmarshalling JSON: ", err)
-		return
-	}
+		if err != nil {
+			fmt.Println("Error reading file", err)
+			return
+		}
 
-	for _, config := range configs {
-		fmt.Printf("ID: %d, Method: %s, URL: %s\n ", config.ID, config.Payload.Method, config.Payload.URL)
+		err = json.Unmarshal(content, &configs)
+		if err != nil {
+			fmt.Println("Error unmarshalling JSON: ", err)
+			return
+		}
+
+		for _, config := range configs {
+			fmt.Printf("ID: %d, Method: %s, URL: %s\n ", config.ID, config.Payload.Method, config.Payload.URL)
+
+			resp, err := http.Get(config.Payload.URL)
+			if err != nil {
+				fmt.Println("Error making HTTP request", err)
+				continue
+			}
+			defer resp.Body.Close()
+
+			fmt.Println("HTTP Response Status: ", resp.Status)
+		}
 	}
 
 }
